a3c56dd1332cf9dbcd879f1fb6403fdca09c0764
sandra <sandra@metafactory.nl>
2021-10-13 17:06:08 +0200

    MS-17 : Add tostring-lombok.xml to blog 3 module

--- blog3-metafactory/src/main/java/org/blog3/pojo/Author.java
+++ blog3-metafactory/src/main/java/org/blog3/pojo/Author.java
@@ -1,14 +1,20 @@
 package org.blog3.pojo;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
- * Author - Created by java/pojo/fields-clean.xml Created by java/pojo/equals-and-hashcode-apache.xml Created by java/pojo/tostring-apache.xml
+ * Author - Created by java/pojo/fields-clean.xml Created by java/pojo/fields-lombok.xml Created by java/pojo/equals-and-hashcode-lombok.xml Created by
+ * java/pojo/tostring-lombok.xml
  * 
  * @author - marnix
  */
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Author {
 
 	private String firstName;
@@ -16,107 +22,5 @@ public class Author {
 	private String lastName;
 
 	private Book book;
-
-	/**
-	 * equals - Fields used as business key: 1) firstName 2) lastName.
-	 * 
-	 * @param other The reference object with which to compare.
-	 * @return boolean Return true if this object is the same as the argument object, otherwise return false.
-	 */
-	@Override
-	public boolean equals(final Object other) {
-		if (this == other) {
-			return true;
-		}
-
-		if (!(other instanceof Author)) {
-			return false;
-		}
-
-		final Author otherAuthor = (Author) other;
-		return new EqualsBuilder().append(this.getFirstName(), otherAuthor.getFirstName())
-
-				.append(this.getLastName(), otherAuthor.getLastName())
-
-				.isEquals();
-	}
-
-	/**
-	 * hashCode - Fields used as business key: 1) firstName 2) lastName.
-	 * 
-	 * @return integer A hash code value for this object.
-	 */
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder().append(this.getFirstName())
-
-				.append(this.getLastName())
-
-				.toHashCode();
-	}
-
-	/**
-	 * toString -
-	 * 
-	 * @return String Return String representation of Author
-	 */
-	@Override
-	public String toString() {
-		return new ToStringBuilder().append("firstName", getFirstName()).append("lastName", getLastName()).toString();
-	}
-
-	/**
-	 * Getter for property firstName.
-	 * 
-	 * @return value of property firstName
-	 */
-	public String getFirstName() {
-		return this.firstName;
-	}
-
-	/**
-	 * Setter for property firstName.
-	 * 
-	 * @param firstName new value of property firstName
-	 */
-	public void setFirstName(final String firstName) {
-		this.firstName = firstName;
-	}
-
-	/**
-	 * Getter for property lastName.
-	 * 
-	 * @return value of property lastName
-	 */
-	public String getLastName() {
-		return this.lastName;
-	}
-
-	/**
-	 * Setter for property lastName.
-	 * 
-	 * @param lastName new value of property lastName
-	 */
-	public void setLastName(final String lastName) {
-		this.lastName = lastName;
-	}
-
-	/**
-	 * Getter for property book.
-	 * 
-	 * @return value of property book
-	 */
-	public Book getBook() {
-		return this.book;
-	}
-
-	/**
-	 * Setter for property book.
-	 * 
-	 * @param book new value of property book
-	 */
-	public void setBook(final Book book) {
-		this.book = book;
-	}
 
 }
--- blog3-metafactory/src/main/java/org/blog3/pojo/Book.java
+++ blog3-metafactory/src/main/java/org/blog3/pojo/Book.java
@@ -1,14 +1,20 @@
 package org.blog3.pojo;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
- * Book - Created by java/pojo/fields-clean.xml Created by java/pojo/equals-and-hashcode-apache.xml Created by java/pojo/tostring-apache.xml
+ * Book - Created by java/pojo/fields-clean.xml Created by java/pojo/fields-lombok.xml Created by java/pojo/equals-and-hashcode-lombok.xml Created by
+ * java/pojo/tostring-lombok.xml
  * 
  * @author - marnix
  */
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class Book {
 
 	private String name;
@@ -16,107 +22,5 @@ public class Book {
 	private String description;
 
 	private Author author;
-
-	/**
-	 * equals - Fields used as business key: 1) name 2) description.
-	 * 
-	 * @param other The reference object with which to compare.
-	 * @return boolean Return true if this object is the same as the argument object, otherwise return false.
-	 */
-	@Override
-	public boolean equals(final Object other) {
-		if (this == other) {
-			return true;
-		}
-
-		if (!(other instanceof Book)) {
-			return false;
-		}
-
-		final Book otherBook = (Book) other;
-		return new EqualsBuilder().append(this.getName(), otherBook.getName())
-
-				.append(this.getDescription(), otherBook.getDescription())
-
-				.isEquals();
-	}
-
-	/**
-	 * hashCode - Fields used as business key: 1) name 2) description.
-	 * 
-	 * @return integer A hash code value for this object.
-	 */
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder().append(this.getName())
-
-				.append(this.getDescription())
-
-				.toHashCode();
-	}
-
-	/**
-	 * toString -
-	 * 
-	 * @return String Return String representation of Book
-	 */
-	@Override
-	public String toString() {
-		return new ToStringBuilder().append("name", getName()).append("description", getDescription()).toString();
-	}
-
-	/**
-	 * Getter for property name.
-	 * 
-	 * @return value of property name
-	 */
-	public String getName() {
-		return this.name;
-	}
-
-	/**
-	 * Setter for property name.
-	 * 
-	 * @param name new value of property name
-	 */
-	public void setName(final String name) {
-		this.name = name;
-	}
-
-	/**
-	 * Getter for property description.
-	 * 
-	 * @return value of property description
-	 */
-	public String getDescription() {
-		return this.description;
-	}
-
-	/**
-	 * Setter for property description.
-	 * 
-	 * @param description new value of property description
-	 */
-	public void setDescription(final String description) {
-		this.description = description;
-	}
-
-	/**
-	 * Getter for property author.
-	 * 
-	 * @return value of property author
-	 */
-	public Author getAuthor() {
-		return this.author;
-	}
-
-	/**
-	 * Setter for property author.
-	 * 
-	 * @param author new value of property author
-	 */
-	public void setAuthor(final Author author) {
-		this.author = author;
-	}
 
 }
